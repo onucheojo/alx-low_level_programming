3e14b4563adf66e9871749113e216ab622ef30c6
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2022-10-29 06:15:03 +0100

    fixing mess 01

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,16 +2,17 @@
 
 /**
  * swap_int - swaps the value of two integers
- * @a - first integer
- * @b - second interger
+ * @a: first integer
+ * @b: second interger
  * return - always zero
  **/
 
-void swap_int( int *a, int *b)
+void swap_int(int *a, int *b)
 {
 	int y, z;
-	y = *a ;
-	z = *b ;
+
+	y = *a;
+	z = *b;
 	*a = z;
 	*b = y;
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _strlen -returns the lenght of a string
- * @s - pointer to the string to be checked
- * return - lenght of string
+ * @s: pointer to the string to be checked
+ * Return: lenght of string
  **/
 int _strlen(char *s)
 {
 	int count, x;
 	char *n;
+
 	count = 0;
 	n = s;
 	x = 0;
