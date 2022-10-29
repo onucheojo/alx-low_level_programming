deacc78718ec2607876350fa7a1dea629ec11d5a
Emmanuel Nwafor <emmanuelnwafor212@gmail.com>
2022-10-29 08:02:35 +0100

    mess fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,17 @@
 */
 int main(void)
 {
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, str1, 59);
-return(1)
+	int x = 0;
+	char *str1;
+
+	str1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	while (str1[x] != '\0')
+	{
+		putchar(str1[x]);
+		x++;
+	}
+	putchar('\n');
+//	write(2, str1, 59);
+	return(1);
 }
+//and that piece of art is useful" - Dora Korpar, 2015-10-19
